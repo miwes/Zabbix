a1d3c3e3ed3db4767d135f43bd9aa9bf0769dffc
miwes <weis@outlook.cz>
2017-01-28 18:12:40 +0100

    Remove

---
+++ Templates/Get-SQLInstance.ps1
@@ -0,0 +1,27 @@
+﻿Param(
+
+    $default = 'false'
+)
+
+$objSQLInstance = Get-Service |? {$_.DisplayName -like 'SQL Server (*'} | Select -ExpandProperty DisplayName
+$Return = ''
+
+ForEach ($objService In $objSQLInstance) {
+    $NameInstance = $objService.Replace('SQL Server (','').Replace(')','')
+    If ($default -eq 'true')
+    {
+        If ($NameInstance -eq 'MSSQLSERVER') {
+            $Return = '{{#SQLINSTANCE}: "SQLServer"}'
+        }
+    } Else {
+        If ($NameInstance -ne 'MSSQLSERVER') {
+            $Return += '{"{#SQLINSTANCE}": "'+$NameInstance+'"},'
+        }
+    }
+}
+If ($Return) {
+    $JSON = $Return  -replace ".{1}$"
+    Write-Host '{"data":[' ; $JSON ; Write-Host ']}'
+} Else {
+    Write-Host '{"data":[{"{#SQLINSTANCE}":"NULL"}]}'
+}
