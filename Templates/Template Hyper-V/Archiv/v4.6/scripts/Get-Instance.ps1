46a590fb623a91835e211351177503279aee3bc3
MiWes <weis@outlook.cz>
2023-05-03 19:31:36 +0200

    Change Hyper-V

--- Templates/Template Hyper-V/Archiv/v4.6/scripts/Get-Instance.ps1
+++
@@ -1,4 +0,0 @@
-Param(
-	[string]$Counter
-)
-('{"data":[' + (Get-Counter -Counter $Counter | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty InstanceName | ForEach {'{"{#INSTANCE}":"' + $_ + '"},'}) + ']}') -Replace ',]}', ']}'
--- Templates/Template Hyper-V/_readme.txt
+++
@@ -1,1 +0,0 @@
-Nutne povolit unsafe v zabbix configu
