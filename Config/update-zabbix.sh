8753169a9b259788970522ab8fbc6f33e74aafc6
MiWes <weis@outlook.cz>
2023-05-03 13:13:21 +0200

    Change name

--- Config/update-zabbix.sh
+++ Config/update-zabbix.sh
@@ -2,7 +2,7 @@
 
 # stahnuti posledniho configurace pro Zabbix agenta
 echo Stahovani ac.conf z Githubu
-wget -O /etc/zabbix/zabbix_agentd.d/ac.conf https://raw.githubusercontent.com/miwes/Zabbix/master/Config/Linux/ac.conf
+wget -O /etc/zabbix/zabbix_agentd.d/ac.conf https://raw.githubusercontent.com/miwes/Zabbix/master/Config/Linux/linux.conf
 
 echo Restart agenta
 systemctl restart zabbix-agent
