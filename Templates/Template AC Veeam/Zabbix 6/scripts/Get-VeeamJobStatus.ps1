636a40fd457e5acbf7d97713294f7a183a9aec0c
MiWes <weis@outlook.cz>
2024-10-07 14:35:48 +0200

    Veeam update
    Veeam update

---
+++ Templates/Template AC Veeam/Zabbix 6/scripts/Get-VeeamJobStatus.ps1
@@ -0,0 +1,92 @@
+<#
+.SYNOPSIS
+    Read information about jobs
+    - History
+        - 1.1 - return XML with all jobs information
+.DESCRIPTION
+.NOTES
+
+.LINK
+.EXAMPLE
+
+#>
+
+<# TODO
+
+#>
+
+[CmdletBinding()]Param (
+     [Parameter(Mandatory=$False)] [Alias("Discovery")] [switch]$attrDiscovery
+)
+
+
+# inicialization
+Set-StrictMode -Version latest
+$global:ErrorActionPreference = 'Stop'
+$Error.Clear()
+
+    
+#$VerbosePreference='Continue'
+    
+# load snappin
+Write-Verbose "[info]; Load VeeamPSSnapin"
+Try 
+{
+    $module = Import-Module Veeam.Backup.PowerShell -ErrorAction stop -Verbose:$false 3> $null
+} 
+Catch 
+{
+    Write-Verbose "[info]; Cannot load VeeamPSSnapin;$($Error[0])"
+    Exit
+}
+
+# return value
+$returnJSON = ''
+
+If ($attrDiscovery) 
+{
+    # discovery mode, list all backup job
+    $returnJSON = '['
+
+    # load jobs
+    Write-Verbose "[info]; Get Veeam jobs"
+    Try 
+    {
+        $jobs = Get-VBRJob
+        #$jobs = [Veeam.Backup.Core.CBackupJob]::GetAll()
+    } 
+    Catch 
+    {
+        Write-Verbose "[info]; Cannot get Veeam jobs;$($Error[0])"
+        Exit
+    }
+
+
+    ForEach($job In $jobs) 
+    {
+        $returnJSON += '{'
+        $returnJSON += '"{#BACKUPJOBNAME}":"' + $job.Name + '",'
+        $description = ($job.Description).Replace('\','\\')
+        $description = $description -replace "`t|`n|`r",""
+        $returnJSON += '"{#BACKUPJOBID}":"' + $job.id + '",'
+        $returnJSON += '"{#BACKUPJOBDESCRIPTION}":"' + $description + '"'
+        $returnJSON += '},'
+    }
+    
+    # remove last ,
+    $returnJSON = $returnJSON -replace ".$"
+    $returnJSON += ']'
+}
+Else
+{
+
+    Try {
+        $lastSession = (Get-VBRJob).findLastSession()
+        $returnJSOn = ($lastSession | ConvertTo-Xml | select OuterXml).OuterXml 
+    }
+    Catch {
+        Write-Verbose "[Error]; $($Error[0])"
+    }
+}
+
+$returnJSON
---
+++ Templates/Template AC Veeam/Zabbix 6/scripts/Get-VeeamRepository.ps1
@@ -0,0 +1,95 @@
+﻿<#
+.SYNOPSIS
+    Read information about repository
+.DESCRIPTION
+.NOTES
+
+.LINK
+.EXAMPLE
+
+#>
+
+<# TODO
+
+#>
+
+[CmdletBinding()]Param (
+     [Parameter(Mandatory=$False)] [Alias("Discovery")]     [switch]$attrDiscovery
+    ,[Parameter(Mandatory=$False)] [Alias("RepositoryID")]  [string]$attrRepositoryID
+)
+
+
+# inicialization
+Set-StrictMode -Version latest
+$global:ErrorActionPreference = 'Stop'
+$Error.Clear()
+
+    
+#$VerbosePreference='Continue'
+    
+# load snappin
+Write-Verbose "[info]; Load VeeamPSSnapin"
+Try 
+{
+    $module = Import-Module Veeam.Backup.PowerShell -ErrorAction stop -Verbose:$false 3> $null
+} 
+Catch 
+{
+    Write-Verbose "[info]; Cannot load VeeamPSSnapin;$($Error[0])"
+    Exit
+}
+
+# return value
+$returnJSON = ''
+
+If ($attrDiscovery) 
+{
+    # discovery mode, list all repository
+    $returnJSON = '['
+
+    # load jobs
+    Write-Verbose "[info]; Get Veeam repository"
+    Try 
+    {
+        $repository =  Get-VBRBackupRepository
+    } 
+    Catch 
+    {
+        Write-Verbose "[info]; Cannot get Veeam repository;$($Error[0])"
+        Exit
+    }
+
+
+    ForEach($repo In $repository) 
+    {
+        $returnJSON += '{'
+        $name = ($repo.Name).Replace('\','\\')
+        $returnJSON += '"{#REPOSITORYNAME}":"' + $name + '",'
+        $description = ($repo.Description).Replace('\','\\')
+        $returnJSON += '"{#REPOSITORYID}":"' + $repo.id + '",'
+        $returnJSON += '"{#REPOSITORYDESCRIPTION}":"' + $description + '"'
+        $returnJSON += '},'
+    }
+    
+    # remove last ,
+    $returnJSON = $returnJSON -replace ".$"
+    $returnJSON += ']'
+}
+ElseIf ($attrRepositoryID)
+{
+    # get Veeam repository, have to use ID -> zabbix now allowed char \ in USERPARAMETER
+    $repo = Get-VBRBackupRepository | Where-Object {$_.id -eq $attrRepositoryID}
+
+    # get usage
+    $repoUsage = $repo | Select-Object @{n='Name';e={$_.Name}},@{n='Type';e={$_.type}},  @{n='Path';e={$_.path}}, @{n='Size';e={$_.GetContainer().CachedTotalSpace.Inbytes}}, @{n='FreeSpace';e={$_.GetContainer().CachedFreeSpace.Inbytes}}
+
+    $returnJSON = '['
+    $returnJSON += '{'
+    $returnJSON += '"REPOSITORYSIZE":"' + $repoUsage.Size + '",'
+    $returnJSON += '"REPOSITORYFREESPACE":"' + $repoUsage.FreeSpace + '"'
+    $returnJSON += '}'
+    $returnJSON += ']'
+
+}
+
+$returnJSON
